e7c949731caebb4dc0a3544920d0bb4edda20649
sebas957 <baxtian957@hotmail.com>
2024-08-27 11:38:07 -0500

    loading en ingresar

--- src/app/modulos/auth/componentes/login/login.component.ts
+++ src/app/modulos/auth/componentes/login/login.component.ts
@@ -1,18 +1,29 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { NbLoginComponent } from '@nebular/auth';
-import { NbAlertModule, NbButtonModule, NbCheckboxModule, NbInputModule } from '@nebular/theme';
-import { TokenService } from '../../servicios/token.service';
-import { AuthService } from '../../servicios/auth.service';
-import { Store } from '@ngrx/store';
-import { usuarioIniciar } from '../../../../redux/actions/usuario.actions';
+import { CommonModule } from "@angular/common";
+import { Component, inject } from "@angular/core";
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { NbLoginComponent } from "@nebular/auth";
+import {
+  NbAlertModule,
+  NbButtonModule,
+  NbCheckboxModule,
+  NbInputModule,
+} from "@nebular/theme";
+import { TokenService } from "../../servicios/token.service";
+import { AuthService } from "../../servicios/auth.service";
+import { Store } from "@ngrx/store";
+import { usuarioIniciar } from "../../../../redux/actions/usuario.actions";
 
 @Component({
-  selector: 'ngx-login',
-  templateUrl: './login.component.html',
-  standalone : true,
+  selector: "ngx-login",
+  templateUrl: "./login.component.html",
+  standalone: true,
   imports: [
     CommonModule,
     FormsModule,
@@ -21,35 +32,54 @@ import { usuarioIniciar } from '../../../../redux/actions/usuario.actions';
     NbInputModule,
     NbButtonModule,
     NbCheckboxModule,
-    ReactiveFormsModule
-  ]
+    ReactiveFormsModule,
+  ],
 })
 export class NgxLoginComponent extends NbLoginComponent {
-
   private tokenService = inject(TokenService);
   private authService = inject(AuthService);
-  private store = inject(Store)
+  private store = inject(Store);
 
   formulario = new FormGroup({
-    username: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(20)]))
-  })
+    username: new FormControl("", Validators.required),
+    password: new FormControl(
+      "",
+      Validators.compose([
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(20),
+      ])
+    ),
+  });
 
-  enviar(){
+  isLoading = false;
+
+  enviar() {
+    if (this.formulario.invalid) {
+      return;
+    }
+    this.isLoading = true; // Mostrar loading
     this.authService.login(this.formulario.value).subscribe(
       (resultado: any) => {
         if (resultado.token) {
           let calcularTiempo = new Date(
             new Date().getTime() + 3 * 60 * 60 * 1000
           );
-          this.store.dispatch(usuarioIniciar({
-            'usuario': resultado.user
-          }))
-          this.tokenService.guardar(resultado.token, calcularTiempo)
-          this.router.navigate(['contenedor']);
+          this.store.dispatch(
+            usuarioIniciar({
+              usuario: resultado.user,
+            })
+          );
+          this.tokenService.guardar(resultado.token, calcularTiempo);
+          this.router.navigate(["contenedor"]);
         }
+      },
+      (error) => {
+        this.isLoading = false;
+      },
+      () => {
+        this.isLoading = false;
       }
     );
   }
-
 }
