a7eac64ac7eced59af0a78999a87850c2b3fdf6f
sebas957 <baxtian957@hotmail.com>
2024-08-28 09:34:24 -0500

    ajustes

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -94,7 +94,7 @@ export class HeaderComponent extends General implements OnInit, OnDestroy {
 
   ejecturarCambioContenedor() {
     this.store.dispatch(ContenedorActionBorrarInformacion())
-    this.router.navigate(['/contenedor/lista']);
+    // this.router.navigate(['/contenedor/lista']);
   }
 
   ngOnDestroy() {
